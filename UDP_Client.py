b37e01e73fd815c1e00f43b4b5bb80deb994cb08
5b 5e 5f 3d 57 65 54 68 65 50 65 6f 70 6c 65 3d 5f 5e 5d <tonssoffunn@gmail.com>
2017-09-21 19:48:22 -0700

    Basic UDP Client
    Minimal functionality...

--- UDP_Client.py
+++ UDP_Client.py
@@ -1,15 +1,17 @@
 import socket
 
-target_host = "127.0.0.1"
-target_port = 80
+# constants
+TARGET_HOST = "127.0.0.1"
+TARGET_PORT = 80
 
-#create a socket object
+# create client socket object from socket class
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#send some data (fuzz)
-client.sendto("AAABBBCCC",(target_host,target_port))
+# send some data (fuzz) to target_host on target_port
+client.sendto("AAABBBCCC",(TARGET_HOST, TARGET_PORT))
 
-#receive some data
+# receive some data
 data, addr = client.recvfrom(4096)
 
+# display received data
 print(data)
