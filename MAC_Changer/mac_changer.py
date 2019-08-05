6dbcba7c7f70fe55a2d30cedca6469fec34cb382
TonsOfFunn <TonsOfFunn@gmail.com>
2019-08-05 04:40:12 -0400

    Added documentation

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -7,7 +7,7 @@
 | Notes:
     MAC address 1st octet must be even number
 | Version: 
-    3
+    4
 | Variables:
     interface
     new_mac
@@ -17,12 +17,19 @@ import subprocess
 import argparse
 
 def get_arguments():
+    '''
+    Get mac_changer.py arguments passed from user input.
+    Returns parser object's stored objects passed from user input.
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address")
     return parser.parse_args()    
 
 def change_mac(interface, new_mac):
+    '''
+    Change mac address of specified interface to new mac address.
+    '''    
     print("[+] Changing MAC address for", interface, "to", new_mac) 
     # Safe practice using lists to store bash commands
     # Can hijack program through code injection " wlan0;ls; "
@@ -30,17 +37,15 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])    
 
-
-#------------[ MAIN ]-------------------
+    # UNsafe practice using string concatenation to store bash commands
+    # Can hijack program through code injection " wlan0;ls; "
+    """
+    subprocess.call("ifconfig " + interface + " down", shell=True)
+    subprocess.call("ifconfig " + interface + " hw ether ", + new_mac, shell=True)
+    subprocess.call("ifconfig " + interface + " up", shell=True)
+    """
+#--------------[ MAIN ]--------------
 
 args = get_arguments()
 change_mac(args.interface, args.new_mac)
 
-
-# UNsafe practice using string concatenation to store bash commands
-# Can hijack program through code injection " wlan0;ls; "
-"""
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether ", + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-"""
