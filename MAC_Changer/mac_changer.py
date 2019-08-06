17f1be84391b9daabfbe1208869eb347b9981bb7
root <root@localhost.localdomain>
2019-08-06 00:08:53 -0400

    Refactored code

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -24,7 +24,13 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()    
+    args = parser.parse_args()    
+    if not args.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    if not args.new_mac:
+        parser.error("[-] Please specify a new mac address, use --help for more info.")
+    return args
+
 
 def change_mac(interface, new_mac):
     '''
@@ -37,13 +43,7 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])    
 
-    # UNsafe practice using string concatenation to store bash commands
-    # Can hijack program through code injection " wlan0;ls; "
-    """
-    subprocess.call("ifconfig " + interface + " down", shell=True)
-    subprocess.call("ifconfig " + interface + " hw ether ", + new_mac, shell=True)
-    subprocess.call("ifconfig " + interface + " up", shell=True)
-    """
+
 #--------------[ MAIN ]--------------
 
 args = get_arguments()
