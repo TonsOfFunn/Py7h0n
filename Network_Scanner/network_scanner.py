ded34b28bd39f6db6a6a8207b9b01a3209e24810
TonsOfFunn <tonssoffunn@gmail.com>
2019-08-09 01:08:57 -0400

    added documentation

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -5,7 +5,7 @@
     Network devices scanner and enumerator to discover clients
     on a network.
 | Version:
-    7
+    8
 | Notes:
     Cannot import scapy using python3
 | Algorithm:
@@ -26,8 +26,27 @@
 """
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    '''
+    Get network_scanner.py arguments passed from user input.
+    Returns parser object's stored objects passed from user input.
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="ip", help="IP to scan")
+    args = parser.parse_args()    
+    if not args.ip:
+        parser.error("[-] Please specify an IP address to scan, use --help for more info.")
+    return args
+
 
 def scan(ip):
+    '''
+    Scans an IP address range given from user in CIDR notation by
+    creating ARP request packet sent to broadcast frame.
+    Returns clients_list containing found IP and MAC addresses.
+    '''
     # create ARP packet
     arp_request = scapy.ARP(pdst=ip)
 
@@ -49,13 +68,23 @@ def scan(ip):
     return clients_list
 
 def print_results(results_list):
-    print("_"*60 + "\nIP\t\t\tMAC Address\n" + "-"*60)
+    '''
+    Displays any scan results found.
+    '''
+    width = 60
+    header = "_" * width
+    footer = "-" * width
+    print(header + "\nIP\t\t\tMAC Address\n" + footer)
     for client in results_list:
         print(client["ip"] + "\t\t" + client["mac"])
 
+
 #--------------[ MAIN ]--------------
 
+# get user input
+args =  get_arguments()
+
 # returns and prints mac address of scanned ip range    
-scan_result = scan("10.0.2.0/24")
+scan_result = scan(args.ip)
 print_results(scan_result)
 
