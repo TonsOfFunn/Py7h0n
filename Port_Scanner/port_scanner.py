19085b5d41860e18b7f534ef9fa1d33a365266fc
TonsOfFunn <tonssoffunn@gmail.com>
2019-08-14 22:51:03 -0400

    added time module and wrote results to file

--- Port_Scanner/port_scanner.py
+++ Port_Scanner/port_scanner.py
@@ -8,7 +8,7 @@
 | Notes:
 
 | Version: 
-    3
+    4
 | Variables:
     nm_scan
     nm_scanner
@@ -20,24 +20,25 @@
 
 import nmap
 import sys
-from pprint import pprint
+import time
 
 nm_scan = nmap.PortScanner()
+
+print("\nRunning...")
+
 nm_scanner = nm_scan.scan(sys.argv[1], '80', arguments='-O')
 
-pprint(nm_scanner)
-
+# parse scan results to variables
 host_up = "The host is: " + nm_scanner['scan'][sys.argv[1]]['status']['state'] + ".\n"
 port_open = "The port 80 is: " + nm_scanner['scan'][sys.argv[1]]['tcp'][80]['state'] + ".\n"
 scan_method = "The method of scanning is: " + nm_scanner['scan'][sys.argv[1]]['tcp'][80]['reason'] + ".\n"
 guessed_os = "There is a %s percent chance that the host is running %s" %(nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['accuracy'], nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['name']) + ".\n"
 
+# write scan results to file
+with open("%s.txt" % sys.argv[1], 'w') as f:
+    f.write(host_up + port_open + scan_method + guessed_os)
+    f.write("\nReport generated " + time.strftime("%Y-%m-%d_%H:%M:%S GMT", time.gmtime()))
+    f.close()
 
-
-
-print(host_up)
-print(port_open)
-print(scan_method)
-print(guessed_os)
-
+print("\nFinished...")
 
