f79bf29f72714536a1fcdf2080993de9dd64ea31
TonsOfFunn <tonssoffunn@gmail.com>
2019-08-14 22:04:59 -0400

    parsed nm_scanner dict results

--- Port_Scanner/port_scanner.py
+++ Port_Scanner/port_scanner.py
@@ -23,4 +23,17 @@ nm_scanner = nm_scan.scan(sys.argv[1], '80', arguments='-O')
 
 pprint(nm_scanner)
 
+host_up = "The host is: " + nm_scanner['scan'][sys.argv[1]]['status']['state'] + ".\n"
+port_open = "The port 80 is: " + nm_scanner['scan'][sys.argv[1]]['tcp'][80]['state'] + ".\n"
+scan_method = "The method of scanning is: " + nm_scanner['scan'][sys.argv[1]]['tcp'][80]['reason'] + ".\n"
+guessed_os = "There is a %s percent chance that the host is running %s" %(nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['accuracy'], nm_scanner['scan'][sys.argv[1]]['osmatch'][0]['name']) + ".\n"
+
+
+
+
+print(host_up)
+print(port_open)
+print(scan_method)
+print(guessed_os)
+
 
